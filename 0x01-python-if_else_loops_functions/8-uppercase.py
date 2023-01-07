b4962df090c159260cb6dce647ffef05ee22c05f
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-07 13:30:52 +0100

    fix: missing hashbang

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,4 @@ def uppercase(str):
     for word in str:
         if ord(word) in range(97, 123):
             word = ord(word) - 32
-            continue
-        print('{:c}'.format(word))
+            print('{:c}'.format(word))
