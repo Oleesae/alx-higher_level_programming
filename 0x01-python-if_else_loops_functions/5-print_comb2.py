e67922d36abe6bccaa378d66383249067277c520
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-07 12:42:31 +0100

    fix: add space after comma

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,5 @@ for num in range(0, 100):
     if num == 99:
         print("{:02d}".format(num))
     else:
-        print('{:02d}'.format(num), end=",")
+        print('{:02d}'.format(num), end=", ")
     
