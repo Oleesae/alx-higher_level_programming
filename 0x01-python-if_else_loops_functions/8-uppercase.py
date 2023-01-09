19771b0c68a69978d0e1a134bff54d0098fcc4f0
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-09 17:04:28 +0100

    fix: not more than 2 loops

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def uppercase(str):
     i = 0
+    num = 0
 
     while i < len(str):
         if ord(str[i]) in range(97, 123):
-            print("{:c}".format(ord(str[i]) - 32), end='')
+            num = 32
         else:
-            print("{:c}".format(ord(str[i])), end='')
+            num = 0
+
+        print("{:c}".format(ord(str[i])), end='')
         i += 1
     else:
         print()
