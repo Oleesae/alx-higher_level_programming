c275e42856668cf7d3538fe14935469448787a26
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-09 17:55:37 +0100

    completed: FizzBuzz

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -14,10 +14,14 @@
 #print(add(98, 0))
 #print(add(100, -2))
 
-pow = __import__('11-pow').pow
+#pow = __import__('11-pow').pow
+#print(pow(2, 2))
+#print(pow(98, 2))
+#print(pow(98, 0))
+#print(pow(100, -2))
+#print(pow(-4, 5))
 
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+fizzbuzz = __import__('12-fizzbuzz').fizzbuzz
+
+fizzbuzz()
+print("")
