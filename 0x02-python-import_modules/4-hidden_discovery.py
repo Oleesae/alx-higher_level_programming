c496155e9c25ba70ab26d0a2f76e786a67cc1051
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-12 18:40:44 +0100

    completed: print hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    for name in dir(hidden_4):
+    from hidden_4 import * as weeb
+    for name in weeb:
 #        if name[0] != "_" and name[1] != "_":
         print(name)
