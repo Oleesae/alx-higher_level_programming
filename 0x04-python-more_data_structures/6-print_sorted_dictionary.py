738a8e23850d4e4fd2081a1d8b6d22e7e06bee3c
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-15 16:28:18 +0100

    completed: print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    dsort = sorted(a_dictionary.keys())
+    for i in range(len(dsort)):
+        if dsort[i] in a_dictionary:
+            print("{}: {}".format(dsort[i], a_dictionary.get(dsort[i])))
