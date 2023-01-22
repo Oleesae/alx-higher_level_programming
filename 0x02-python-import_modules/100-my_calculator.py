63eedb3609675afa33cad55195f49be40692ccf9
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-23 00:04:37 +0100

    completed: calculator operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    argv = sys.argv
+    argl = len(argv)
+
+    if argl == 4:
+        a = int(argv[1])
+        b = int(argv[3])
+        op = argv[2]
+        if op == '+':
+            print("{} {} {} = {}".format(a, op, b, add(a, b)))
+        elif op == '-':
+            print("{} {} {} = {}".format(a, op, b, sub(a, b)))
+        elif op == '*':
+            print("{:d} {} {:d} = {}".format(a, op, b, mul(a, b)))
+        elif op == '/':
+            print("{} {} {} = {}".format(a, op, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
