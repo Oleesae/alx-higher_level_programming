cb464f9a9c2e48010dc7dad92445df695bed4a00
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-24 21:53:44 +0100

    completed: safe function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_function(fct, *args):
     try:
         ret = fct(*args)
     except Exception as e:
         ret = None
-        print("Exception: {}".format(e))
+        print("Exception: {}".format(e), file=sys.stderr)
     finally:
         return ret
