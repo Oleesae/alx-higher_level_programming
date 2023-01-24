80e639e782fbf86d3dd4994b4063ccd512a28e6e
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-24 02:33:32 +0100

    completed: try integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)))
         return True
-    except ValueError:
+    except Exception:
+        print(Exception)
         return False
