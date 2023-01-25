02e937248062ffc8e2ab946322a493dbbfd9167d
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-25 14:09:49 +0100

    completed: size definition

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A Square class."""
+
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size):
+        """Initializes the size of the square"""
+        self.__size = size
