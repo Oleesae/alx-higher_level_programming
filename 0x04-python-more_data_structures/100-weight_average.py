431e4f7bffa6d45e6fe568d43d28eddbdd25db31
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-01-26 21:19:52 +0100

    completed: weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,10 +3,9 @@ def weight_average(my_list=[]):
     num = 0
     den = 0
 
-    if not my:
+    if not my_list:
         return 0
     for i in my_list:
         num += i[0] * i[1]
         den += i[1]
-
     return num / den
