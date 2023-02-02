69a1f35414577e3706a45a27e5971e286b9b8b68
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-03 00:40:02 +0100

    first test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,7 +12,8 @@ def add_integer(a, b=98):
     """
     if not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, (float, int)):
+    if not isinstance(b, (float, int)):
         raise TypeError("b must be an integer")
 
+    
     return int(a) + int(b)
