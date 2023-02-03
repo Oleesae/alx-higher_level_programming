53e41d95e6907a7a80aca736ceae9d584dc6c685
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-03 16:18:40 +0100

    fourth test

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+
+Print square module
+
+"""
+
+
+def print_square(size):
+    """ (int) -> char(#)
+
+    Returns a square of size 'size' printed with #.
+    size must be an integer.
+
+    Precondition: size >= 0
+    """
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for row in range(size):
+        for col in range(size):
+            print("#", end="")
+        if row != size - 1:
+            print()
+    print()
