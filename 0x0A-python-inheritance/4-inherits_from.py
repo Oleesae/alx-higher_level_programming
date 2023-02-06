b44335b1505ff962148eaf8fd5f3b899bad2c0a7
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-07 00:03:56 +0100

    same

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+inherits_from module
+
+"""
+
+
+def inherits_from(obj, a_class):
+    """ object, class -> bool
+
+    Returns True if the obj is exactly an instance
+    of the specified class; otherwise False.
+    """
+
+    return isinstance(type(obj), a_class) and type(obj) != a_class
