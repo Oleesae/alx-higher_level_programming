24e6da251ee52d38cb919e90d41b41782c1bb57f
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-06 23:42:04 +0100

    second

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -14,10 +14,8 @@ class MyList(list):
 
     def __init__(self):
         """ Initializes object """
-
         super().__init__()
 
     def print_sorted(self):
         """ Prints a sorted list """
-
         print(sorted(self.copy()))
