ec149dfd4b259a3fc560982eb85b43b7203ab042
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-06 22:12:15 +0100

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A lookup module
+
+"""
+
+def lookup(obj):
+    """ obj -> list
+
+    Returns a list of available attributes
+    and methods of an object
+    """
+    return dir(obj)
