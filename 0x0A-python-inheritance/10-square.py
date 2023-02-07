6aca24d6d2b8524a90f2543897247d54af1965b3
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-07 13:54:18 +0100

    Square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A Square class derived from A Rectangle class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Defines a Square with a Rectangle parent class
+    """
+
+    def __init__(self, size):
+        """ Initializes an instance of a Square object
+        with Rectangle attributes
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
