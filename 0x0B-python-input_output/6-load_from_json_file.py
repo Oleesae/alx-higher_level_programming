e84ed13acf8d121ea8661cddcb01076f61bf7ffc
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-08 23:16:17 +0100

    write into a file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Object file to json function"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """file object -> json object
+
+    Creates an Object from a JSON file
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        string = f.read()
+        obj = json.loads(string)
+        return obj
