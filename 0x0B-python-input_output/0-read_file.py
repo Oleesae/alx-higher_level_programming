946f742a1f4e5e5701abf06c7575ff9c1cfced3f
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-08 14:40:09 +0100

    check read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,6 @@ def read_file(filename=""):
 
     Reads a file
     """
-    with open(filename, encoding="UTF8") as f:
+    with open(filename, encoding="utf-8") as f:
         read_file = f.read()
     return read_file
