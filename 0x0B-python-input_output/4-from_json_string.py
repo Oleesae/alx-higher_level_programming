b2e8fe53fd9cc417762a087833ed9e2aaccfc1e0
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-08 22:46:52 +0100

    write into a file

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Converts JSON string to Object """
+
+
+def from_json_string(my_str):
+    """str -> object
+
+    Returns an object(Python data structure)
+    represented by a JSON string
+    """
+
+    return json.loads(my_str)
