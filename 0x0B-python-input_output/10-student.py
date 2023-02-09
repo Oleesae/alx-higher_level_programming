1145e6665ca52d1fa4e4b5621b4a194ee4a10c8e
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-09 01:22:34 +0100

    student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A Student class"""
+
+
+class Student:
+    """Defines a Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes an instance of the student object"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary representation of the object"""
+
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
