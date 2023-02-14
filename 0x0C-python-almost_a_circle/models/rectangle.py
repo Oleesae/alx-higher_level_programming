3efb0d8a8c1c4c890fa21bbf27708f2fd9659de3
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-14 02:02:31 +0100

    rectanlge validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,6 +25,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """updates the value of the width attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 1:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -35,6 +39,11 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """updates the value of the height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 1:
+            raise ValueError("height must be > 0")
+
         self.__height = value
 
     @property
@@ -45,6 +54,11 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """updates the value of x"""
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
         self.__x = value
 
     @property
@@ -55,4 +69,8 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """updates the value of y"""
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
