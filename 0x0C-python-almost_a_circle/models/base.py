0cc346ef3509df4d509fa7237eaccc1843a8b2e4
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-14 01:13:03 +0100

    base file

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Base class
+
+Manages id attribute
+"""
+
+
+class Base:
+    """ A Base class managing the number
+    of object instances
+    """
+
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Initializes an object"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
