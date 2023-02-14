67a15c4eb9c91bc47e64d801517438ba52a28e77
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-14 01:52:59 +0100

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """A rectangle Class"""
-Base = __import__('base').Base
+from models.base import Base
 
 
 class Rectangle(Base):
@@ -11,7 +11,7 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initializes an object instance """
 
-        super().__init__()
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
