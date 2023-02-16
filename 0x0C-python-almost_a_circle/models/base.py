ab02cdcb8e3b11670e7b511216d3d3a87da649e3
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-16 10:54:16 +0100

    test json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -28,7 +28,20 @@ class Base:
         dictionary
         """
         if list_dictionaries is None or list_dictionaries == []:
-            j_str = json.dumps([])
+            return "[]"
 
         j_str = json.dumps(list_dictionaries)
         return j_str
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the json string repersentation
+        of list_objs to a file
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w+', encoding="utf-8") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                dict_list = [obj.to_dictionary() for obj in list_objs]
+                f.write(Base.to_json_string(dict_list))
