923aae3ee66554e150dac917c1063b7e69bfdf8c
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-02-23 00:28:42 +0100

    base instantiation test

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Unitests for the Base class
+"""
+import unittest
+import os
+from models.rectangle import Rectangle
+from models.base import Base
+from models.square import Square
+
+
+class TestBaseInstantiation(unittest.TestCase):
+    """
+    A test case for the Initialization of the
+    Base class
+    """
+
+    def setUp(self):
+        self.b1 = Base()
+        self.b2 = Base()
+        self.b3 = Base(89)
+        self.b4 = Base()
+
+    def tearDown(self):
+        pass
+
+    def test_no_args(self):
+        self.assertEqual(self.b1.id, self.b2.id - 1)
+
+    def test_unique(self):
+        self.assertEqual(self.b3.id, 89)
+
+    def test_mixed_id(self):
+        self.assertEqual(self.b2.id, self.b4.id - 1)
