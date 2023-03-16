b0addef760ea829046523c3a6bb6aaa40475636a
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-03-16 23:46:12 +0100

    best score

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- Lists records by column score, name
+-- where score is greater than or equal to 10
+-- in descending order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
