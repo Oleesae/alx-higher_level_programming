14d9398a86eb93e89dceed868146af27478e47b4
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-03-22 21:26:38 +0100

    tv shows

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- lists all shows contained in a database that have at least one genre
+SELECT title, genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id
+ASC;
