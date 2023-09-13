2a47d33df331d8d9afacc2611a8f90c5ca2e6b30
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-09-13 13:29:50 +0100

    search replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,5 @@ def search_replace(my_list, search, replace):
     new = my_list.copy()
     for i in range(len(new)):
         if new[i] == search:
-            new.pop(i)
-            new.insert(i, replace)
+            new[i] = replace
     return (new)
