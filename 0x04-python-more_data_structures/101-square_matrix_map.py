e48afb4971894ee9e7e73ef9f5a3d060bd666d80
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-09-13 14:28:08 +0100

    square map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x**2, j)) for j in matrix]
+    return list(map(lambda x: list(map(lambda j: j**2, x)), matrix))
