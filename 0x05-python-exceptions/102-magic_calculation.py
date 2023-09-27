11ce0195289bcb1fa7da411bb2ec18ab9606106e
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-09-27 11:54:37 +0100

    magic

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,23 @@
+#!/bin/bash/python3
+def magic_calculation(a, b):
+    """Computes the power-divided sum of a and b over the range of 1 to 3
+
+    Args:
+    a: A float
+    b: A float
+
+    Returns:
+    A float
+    """
+
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / i
+        except:
+            result = a + b
+            break
+    return (result)
