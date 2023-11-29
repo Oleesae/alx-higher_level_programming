83047dbcab224a371f9d5ece1fc76f8dcaa1d086
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-11-29 14:01:33 +0100

    fix: select error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- convert hbtn_0c_0 to UTF8
+USE hbtn_0c_0;
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
 ALTER TABLE users MODIFY name VARCHAR(256) CHARACTER SET utf8 COLLATE utf8_general_ci;
