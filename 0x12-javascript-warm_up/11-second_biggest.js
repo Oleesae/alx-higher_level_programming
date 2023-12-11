465e943bcca2dfdcb91e7535570de278fff3066c
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-12-11 18:45:48 +0100

    first 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,15 +2,13 @@
 
 const { argv } = require('node:process');
 
-let argArray = argv.slice(2,);
-let largest;
+const argArray = argv.slice(2, argv.length);
 let secLargest;
 if (argv[2] === undefined || argv.length === 3) {
   console.log(0);
 } else {
   argArray.sort();
-  largest = Math.max(...argArray);
-  secLargest = argArray[argArray.length - 3, argArray.length-2];
+  secLargest = argArray[argArray.length - 2];
 }
 
 console.log(secLargest);
