f20947828ef47d236818f82f1eb5241d2b20fc61
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-12-11 21:33:21 +0100

    fix: 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -9,6 +9,9 @@ if (argv[2] === undefined || argv.length === 3) {
 } else {
   argArray.sort();
   secLargest = argArray[argArray.length - 2];
+  if (secLargest === argArray[argArray.length - 1]) {
+    secLargest = argArray[argArray.length - 3];
+  }
 }
 
 console.log(secLargest);
