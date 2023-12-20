1b8f82b4a881f9c48ad2ea8604aa2a31ca1a5b6c
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-12-20 19:47:51 +0100

    add:cities_by_state

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A script that lists all states in a database
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username, password, name = sys.argv[1:]
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=username,
+        passwd=password, db=name, charset="utf8"
+    )
+    cur = conn.cursor()
+    query = "SELECT cities.id, cities.name, states.name\
+    FROM cities INNER JOIN states \
+    ON cities.state_id = states.id order by cities.id asc;"
+    cur.execute(query)
+    query_rows = cur.fetchall()
+    for state in query_rows:
+        print(state)
+    cur.close()
+    conn.close()
