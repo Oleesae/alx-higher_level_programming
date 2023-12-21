d557111dac188962aa3055a4b09231a015bbad7f
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-12-21 01:14:45 +0100

    fix:style

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,9 +7,9 @@ Base = declarative_base()
 
 
 class State(Base):
-        """
-        The State Model
-        """
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True)
-        name =  Column(String(50))
+    """
+    The State Model
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
