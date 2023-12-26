69cf162ad09cd637a2e270c0be09008db2c441b2
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2023-12-26 01:22:13 +0100

    add:delete_a

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A Script that lists all State objects from a database."""
+
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    import sys
+
+    host, password, name = sys.argv[1:]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(host, password, name),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = sessionmaker(bind=engine)()
+    for state in session.query(State).order_by(State.name.like('%a%')):
+        session.delete(state)
+    session.commit()
+    session.close()
