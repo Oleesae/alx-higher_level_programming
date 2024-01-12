c8e0df44bbbaf0b7d084e4b3de814078d4b120c3
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2024-01-12 17:22:03 +0100

    add:api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
-"""Takes in a letter and sends a POST request
-to http://0.0.0.0:5000/search_user with the
-letter as a parameter.
+"""takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter
+as a parameter.
 """
 import requests
 import sys
 
 
-if __name__ == "__main__":
-    """Takes in a letter and sends a POST request
-    to http://0.0.0.0:5000/search_user with the
-    letter as a parameter.
+def main():
+    """takes in a letter and sends a POST request
+    to http://0.0.0.0:5000/search_user with the letter
+    as a parameter.
     """
     url = 'http://0.0.0.0:5000/search_user'
     if len(sys.argv) < 2:
-        data = {'q': ""}
+        data_load = {'q': ""}
     else:
-        data = {'q': sys.argv[1]}
+        data_load = {'q': sys.argv[1]}
 
-    r = requests.post(url, data)
+    r = requests.post(url, data_load)
 
     try:
-        dump_r = r.json()
+        r_dumped = r.json()
     except ValueError as e:
         print("Not a valid JSON")
         return
 
-    if len(dump_r) == 0:
+    if len(r_dumped) == 0:
         print("No result")
         return
 
     print("[{}] {}".format(r_dumped['id'], r_dumped['name']))
+
+
+if __name__ == "__main__":
+    main()
