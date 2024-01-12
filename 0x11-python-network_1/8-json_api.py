98ca6c1a754554fee0a1854bb5915d8e3eececdf
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2024-01-12 17:20:17 +0100

    add:api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,6 +8,10 @@ import sys
 
 
 if __name__ == "__main__":
+    """Takes in a letter and sends a POST request
+    to http://0.0.0.0:5000/search_user with the
+    letter as a parameter.
+    """
     url = 'http://0.0.0.0:5000/search_user'
     if len(sys.argv) < 2:
         data = {'q': ""}
