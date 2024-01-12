16e0c6e835cb2da0439b96aecf872b27f9840b2b
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2024-01-12 16:54:52 +0100

    add:response-email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,7 +11,7 @@ def main():
     url = sys.argv[1]
     headers = {'email': sys.argv[2]}
 
-    r = requests.get(url, heeaders=headers)
+    r = requests.get(url, headers=headers)
     print(r.headers['email'])
 
 
