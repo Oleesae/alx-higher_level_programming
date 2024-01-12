1482b2a8f68c41e2c2526c7ad150b60658a1cff0
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2024-01-12 17:17:50 +0100

    add:github-api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,6 +4,7 @@ This script takes your GitHub credentials (username and password)
 and uses the GitHub API to display your id.
 """
 
+
 if __name__ == "__main__":
     import sys
     import requests
@@ -17,8 +18,7 @@ if __name__ == "__main__":
         "Accept": "application/vnd.github+json",
         "Authorization": f"Bearer {token}",
         "X-Github-Api-Version": "2022-11-28"
-
-}
+    }
     # construct url
     url = f"https://api.github.com/user"
     with requests.get(url, headers=headers) as response:
