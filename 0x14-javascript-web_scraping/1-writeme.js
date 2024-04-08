c24031ef5869ce921a3877debe78cc050e17de59
Eboka Olisaemeka <ebokaolisaemeka@gmail.com>
2024-04-08 10:51:10 +0100

    fix:errors

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,5 +2,5 @@
 // writes a string to a file
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], error => {
-    if (error) console.log(error);
+  if (error) console.log(error);
 });
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 // prints the status code of a GET request
 const request = require('request');
 request.get(process.argv[2]).on('response', function (response) {
-    console.log(`code: ${response.statusCode}`);
+  console.log(`code: ${response.statusCode}`);
 });
